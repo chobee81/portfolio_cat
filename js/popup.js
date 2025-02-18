1b8a1e483dbceb27f07c753965efd16f5fb96658
chobee81 <54051054+chobee81@users.noreply.github.com>
2025-02-18 12:45:18 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+﻿// JavaScript Document
+
+
+$(document).ready(function() {
+    $('div#popup').hide();
+	function setCookie(name, value, expiredays) {
+		var todayDate = new Date();
+		todayDate.setDate( todayDate.getDate() + expiredays );
+		document.cookie = name + '=' + escape( value ) + '; path=/; expires=' + todayDate.toGMTString() + ';'
+	};
+
+	function closeWin() {
+		if ( $('#chkbox').prop('checked') == true){
+			setCookie( 'maindiv', 'done', 1 );
+		}
+		$('div#popup').slideUp('fast');
+	};
+
+	cookiedata = document.cookie;
+	if(cookiedata.indexOf('maindiv=done') < 0 ){
+		$('div#popup').slideDown();
+	}else{
+		$('div#popup').hide();
+	}
+
+	$('div#popup a').click(function() {
+		closeWin();
+	});
+	$(window).scroll(function() {
+		$('div#popup').hide();
+    });
+});
